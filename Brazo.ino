a1c0f516ddb3a3ac9649d8f36028fca406720f4b
Teogrone <teogrone@hotmail.com>
2018-05-20 23:56:45 -0500

    Recoge, muestra y entrega

--- Brazo.ino
+++ Brazo.ino
@@ -1,8 +1,8 @@
 #include <Servo.h>
 #define pinbase 11
-#define pinladoi 8
-#define pinladod 10
-#define pingarra 9
+#define pinladoi 7
+#define pinladod 4
+#define pingarra 12
 #define initBase 90
 
 Servo base;
@@ -17,19 +17,80 @@ base.attach (pinbase);
 ladoi.attach (pinladoi);
 ladod.attach (pinladod);
 garra.attach (pingarra);
+
+  base.write (val=initBase +25);
+  delay (5000);
+  garra.write (val=initBase +110);
+  delay (2000);
+  ladod.write (val=initBase -90);
+  delay (2000);
+  base.write(val=initBase -40);
+  delay (6000);
+  base.write(val=initBase -80);
+  delay (2000);
+  ladod.write(val=initBase +0);
+  delay (2000);
+  garra.write(val=initBase -20);
+  delay (2000);
+  base.write (val=initBase +25);
+  delay (5000);
+  garra.write (val=initBase +110);
+  delay (2000);
+  ladod.write (val=initBase -90);
+  delay (2000);
+  base.write(val=initBase -40);
+  delay (6000);
+  base.write(val=initBase -80);
+  delay (2000);
+  ladod.write(val=initBase +0);
+  delay (2000);
+  garra.write(val=initBase -20);
+  delay (2000);
+  base.write (val=initBase +25);
+  delay (5000);
+  garra.write (val=initBase +110);
+  delay (2000);
+  ladod.write (val=initBase -90);
+  delay (2000);
+  base.write(val=initBase -40);
+  delay (6000);
+  base.write(val=initBase -80);
+  delay (2000);
+  ladod.write(val=initBase +0);
+  delay (2000);
+  garra.write(val=initBase -20);
+  delay (2000);
+  base.write (val=initBase +25);
+  delay (5000);
+  garra.write (val=initBase +110);
+  delay (2000);
+  ladod.write (val=initBase -90);
+  delay (2000);
+  base.write(val=initBase -40);
+  delay (6000);
+  base.write(val=initBase -80);
+  delay (2000);
+  ladod.write(val=initBase +0);
+  delay (2000);
+  garra.write(val=initBase -20);
+  delay (2000);
+  base.write (val=initBase +25);
+  delay (5000);
+  garra.write (val=initBase +110);
+  delay (2000);
+  ladod.write (val=initBase -90);
+  delay (2000);
+  base.write(val=initBase -40);
+  delay (6000);
+  base.write(val=initBase -80);
+  delay (2000);
+  ladod.write(val=initBase +0);
+  delay (2000);
+  garra.write(val=initBase -20);
+  delay (2000);
+  
 }
 
 void loop() {
-  base.write (val=initBase +50);
-  delay (2000);
-  ladod.write (val=initBase +50);
-  delay (2000);
-  ladoi.write (val=initBase +50);
-  delay (2000);
-  base.write (val=initBase -50); 
-  delay (2000);
-  ladoi.write (val=initBase -50);
-  delay (2000);
-  ladod.write (val=initBase -50);
-  delay (2000);
+
   }
