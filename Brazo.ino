54abf5b4eb006b28dcc8d236a8373b4b1caf9beb
Teogrone <teogrone@hotmail.com>
2018-05-28 13:27:34 -0500

    Listo para la muestra tecnológica.

--- Brazo.ino
+++ Brazo.ino
@@ -1,8 +1,14 @@
 #include <Servo.h>
-#define pinbase 11
-#define pinladoi 7
-#define pinladod 4
+#define pinbase 13
+#define pinladod 10
+#define pinladoi 11
 #define pingarra 12
+#define pinboton 6
+#define pinboton2 5
+#define pinboton3 3
+#define pinboton4 2
+#define pinboton5 7
+#define pinboton5 8
 #define initBase 90
 
 Servo base;
@@ -11,12 +17,25 @@ Servo ladod;
 Servo garra;
 
 int val;
+int boton=0;
+int boton2=0;
+int boton3=0;
+int boton4=0;
+int boton5=0;
+int boton6=0;
 
 void setup() {
 base.attach (pinbase);
 ladoi.attach (pinladoi);
 ladod.attach (pinladod);
 garra.attach (pingarra);
+
+pinMode (6,INPUT);
+pinMode (5,INPUT);
+pinMode (4,INPUT);
+pinMode (3,INPUT);
+pinMode (7,INPUT);
+pinMode (8,INPUT);
 
   base.write (val=initBase +90);
   delay (4000);
@@ -110,5 +129,34 @@ garra.attach (pingarra);
 }
 
 void loop() {
-
+  boton=digitalRead(6);
+  if (boton==HIGH){
+    ladod.write(val= initBase +45);
+    delay(10);
+    }
+  boton2=digitalRead(5);
+  if (boton2==HIGH){
+    ladod.write(val= initBase -90);
+    delay(10);
+    }
+  boton3=digitalRead(3);
+  if (boton3==HIGH){
+    base.write(val= initBase -30);
+    delay(10);
+    }
+  boton4=digitalRead(2);
+  if (boton4==HIGH){
+    base.write(val= initBase +100);
+    delay(10);
+    }
+  boton5=digitalRead(7);
+  if (boton5==HIGH){
+    garra.write(val= initBase +120);
+    delay(10);
+    }
+  boton6=digitalRead(8);
+  if (boton6==HIGH){
+    garra.write(val= initBase +30);
+    delay(10);
+    }
   }
